f4d699170502a880cac40f509d6144625c25e1cc
Macintosh_PLM <lovemarckendyparvilus@gmail.com>
2022-11-28 14:16:37 -0500

    14eme sauvegarde

--- src/fragments/FragRecipes.js
+++ src/fragments/FragRecipes.js
@@ -4,7 +4,12 @@ import './styles/FragRecipes.scss'
 function FragRecipes() {
   return (
     <Fragment>
-      
+      <section className="recipes--index">
+        <div className="content--container"></div>
+        <section className="recipes--index-video"></section>
+        <div className="content--container"></div>
+      </section>
+      <div className="clearfix"></div>
     </Fragment>
   )
 }
